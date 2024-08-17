1cf6f02f21218040d50545f1990c2b229674bf5f
Matthew Bennett <mattsbennett@gmail.com>
2024-08-17 15:52:42 -0700

    Add logo, header links, hero styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,19 +15,24 @@ import s from './page.module.scss'
 import { Switch } from './components/Switch'
 import DarkModeSwitch from './components/DarkModeSwitch'
 import { Suspense } from 'react'
+import Link from 'next/link'
+import { Card, CardHeader, CardContent, CardDescription } from './components/Card'
 
 export default function Home() {
   return (
     <main className={s.main}>
       <header className={s.header}>
-        <div>Logo</div>
-        <div>
+        <div className={s.logoWrap}><Link href={'/'}><Image src={'/logo.svg'} width={45} height={45} alt='Logo'/></Link></div>
+        <div className={s.navigation}>
+          <Link href='/about'>About</Link>
+          <Link href='/about'>Contact</Link>
           <DarkModeSwitch />
         </div>
       </header>
       <section className={s.hero}>
-        <h1>How we got our Bible</h1>
+        <h1>How we got our <span className={s.underlined}>Bible</span></h1>
         <h2>A journey through time</h2>
+        <Card className={s.card}><CardDescription className={s.cardDescription}>This site is dedicated to the miracle of the Bible. We will endeavour to provide the reader with a sketch of the major events that have shaped the history of the Bible. We will review some of the processes, people and places of Bible history.</CardDescription></Card>
       </section>
       <section className={s.timeline}>
         <div className={s.timelineComponent}>
