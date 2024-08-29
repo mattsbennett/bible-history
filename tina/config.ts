41a0d7663f0dafc4fdca88ff10c7e833d65a25c8
Matthew Bennett <mattsbennett@gmail.com>
2024-08-28 20:35:36 -0700

    Add tina cloud config

--- tina/config.ts
+++ tina/config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from "tinacms";
 
 // Your hosting provider likely exposes this as an environment variable
 const branch =
+  process.env.NEXT_PUBLIC_TINA_BRANCH ||
+  process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF ||
   process.env.GITHUB_BRANCH ||
   process.env.VERCEL_GIT_COMMIT_REF ||
   process.env.HEAD ||
